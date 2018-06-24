60cccb6fe7512144c5d991971e35add61e73db08
Patty <pattyvaderbr@gmail.com>
2018-06-24 22:25:40 +0200

    Add initial infrastructure file

---
+++ sns_sqs.py
@@ -0,0 +1,56 @@
+from troposphere import GetAtt, Output, Parameter, Ref, Template
+from troposphere.sns import Subscription, Topic
+from troposphere.sqs import Queue, RedrivePolicy, QueuePolicy
+
+template = Template()
+
+template.add_description(
+    "AWS CloudFormation Sample Template SNS_AND_SQS_: Sample")
+
+dead_letter_queue = template.add_resource(Queue(
+    "deadSQS", 
+    QueueName="dead__letter_queue__iac_sqs_sample"
+    )
+)
+
+sqs_aws = template.add_resource(Queue(
+    "iacSQS",
+    QueueName="iac_sqs_sample",
+    RedrivePolicy=RedrivePolicy(
+        deadLetterTargetArn=GetAtt(dead_letter_queue, "Arn"),
+        maxReceiveCount="5",
+    )
+))
+
+sns_aws = template.add_resource(Topic("iacSNS",
+    TopicName="iac_sns_sample",
+    Subscription=[Subscription(
+        Protocol="sqs",
+        Endpoint=GetAtt(sqs_aws, "Arn")
+    )]
+))
+
+template.add_output([
+    Output(
+        "SourceQueueURL",
+        Description="URL of the source queue",
+        Value=Ref(sqs_aws)
+    ),
+    Output(
+        "SourceQueueARN",
+        Description="ARN of the source queue",
+        Value=GetAtt(sqs_aws, "Arn")
+    ),
+    Output(
+        "DeadLetterQueueURL",
+        Description="URL of the dead letter queue",
+        Value=Ref(dead_letter_queue)
+    ),
+    Output(
+        "DeadLetterQueueARN",
+        Description="ARN of the dead letter queue",
+        Value=GetAtt(dead_letter_queue, "Arn")
+    ),
+])
+
+print(template.to_json())
